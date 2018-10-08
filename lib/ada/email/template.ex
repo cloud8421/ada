d9d4937d81ddd23f62ffbac9a8029e5303006a15
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-10-08 17:10:02 +0100

    [F] Add email template for news
    Imported from Ada v1.

--- lib/ada/email/template.ex
+++ lib/ada/email/template.ex
@@ -19,12 +19,23 @@ defmodule Ada.Email.Template do
     [:summary, :data_points]
   )
 
+  EEx.function_from_file(
+    :def,
+    :stories_template,
+    Path.join(@templates_path, "stories.html.eex"),
+    [:stories]
+  )
+
   def weather(location_name, report) do
     layout_template(
       @style_css,
       "Weather for #{location_name}",
       weather_template(report.summary, report.data_points)
     )
+  end
+
+  def news(source_name, stories) do
+    layout_template(@style_css, "News for #{source_name}", stories_template(stories))
   end
 
   defp format_weather_datetime(datetime) do
