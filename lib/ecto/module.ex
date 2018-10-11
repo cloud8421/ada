0f0303ecf0048c3556e22bedee3f2238dd434af4
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-10-11 08:55:54 +0100

    [F] Allow casting workflow name from string
    Allows passing a workflow name in 3 forms:
    
    - Ada.Workflow.Foo
    - “Ada.Workflow.Foo”
    - “Elixir.Ada.Workflow.Foo”

--- lib/ecto/module.ex
+++ lib/ecto/module.ex
@@ -4,6 +4,15 @@ defmodule Ecto.Module do
   def type, do: :string
 
   def cast(value) when is_atom(value), do: {:ok, value}
+
+  def cast("Elixir." <> _rest = mod_name) do
+    {:ok, String.to_existing_atom(mod_name)}
+  end
+
+  def cast(mod_name_without_prefix) when is_binary(mod_name_without_prefix) do
+    {:ok, String.to_existing_atom("Elixir." <> mod_name_without_prefix)}
+  end
+
   def cast(_), do: :error
 
   def load(value), do: {:ok, String.to_existing_atom(value)}
