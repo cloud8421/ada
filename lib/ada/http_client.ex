94ee6b92efd51e78d595e4e4be1a35c407fd9a9d
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-10-11 16:31:09 +0100

    [F] Extend http client
    - add put
    - add delete (no body)
    - Shorthands for json get, post and put

--- lib/ada/http_client.ex
+++ lib/ada/http_client.ex
@@ -15,6 +15,42 @@ defmodule Ada.HTTPClient do
     defstruct message: nil
   end
 
+  def json_get(url, headers \\ [], qs_params \\ []) do
+    case get(url, headers, qs_params) do
+      %Response{} = response ->
+        %{response | body: Jason.decode!(response.body)}
+
+      error_response ->
+        error_response
+    end
+  end
+
+  def json_post(url, data, headers \\ []) do
+    case post(url, Jason.encode!(data), headers, 'application/json') do
+      %Response{body: []} = response ->
+        response
+
+      %Response{} = response ->
+        %{response | body: Jason.decode!(response.body)}
+
+      error_response ->
+        error_response
+    end
+  end
+
+  def json_put(url, data, headers \\ []) do
+    case put(url, Jason.encode!(data), headers, 'application/json') do
+      %Response{body: []} = response ->
+        response
+
+      %Response{} = response ->
+        %{response | body: Jason.decode!(response.body)}
+
+      error_response ->
+        error_response
+    end
+  end
+
   def get(url, headers \\ [], qs_params \\ %{}) do
     headers =
       Enum.map(headers, fn {k, v} ->
@@ -34,6 +70,26 @@ defmodule Ada.HTTPClient do
       end)
 
     :httpc.request(:post, {String.to_charlist(url), headers, content_type, body}, [], [])
+    |> process_response
+  end
+
+  def put(url, body, headers \\ [], content_type \\ 'application/json') do
+    headers =
+      Enum.map(headers, fn {k, v} ->
+        {String.to_charlist(k), String.to_charlist(v)}
+      end)
+
+    :httpc.request(:put, {String.to_charlist(url), headers, content_type, body}, [], [])
+    |> process_response
+  end
+
+  def delete(url, headers \\ []) do
+    headers =
+      Enum.map(headers, fn {k, v} ->
+        {String.to_charlist(k), String.to_charlist(v)}
+      end)
+
+    :httpc.request(:delete, {String.to_charlist(url), headers, [], []}, [], [])
     |> process_response
   end
 
