7161af0e9ace25ac81d0b424a4e2cd5d4b5eac1c
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-10-11 10:39:29 +0100

    [R] Extract router helpers for readability

--- lib/ada/http/router.ex
+++ lib/ada/http/router.ex
@@ -5,19 +5,24 @@ defmodule Ada.HTTP.Router do
     :cowboy_router.compile([
       {:_,
        [
-         {'/locations', Handler.Collection, Keyword.put(opts, :schema, Ada.Schema.Location)},
-         {'/locations/[:resource_id]', Handler.Resource,
-          Keyword.put(opts, :schema, Ada.Schema.Location)},
-         {'/users', Handler.Collection, Keyword.put(opts, :schema, Ada.Schema.User)},
-         {'/users/[:resource_id]', Handler.Resource, Keyword.put(opts, :schema, Ada.Schema.User)},
-         {'/scheduled_tasks', Handler.Collection,
-          Keyword.put(opts, :schema, Ada.Schema.ScheduledTask)},
-         {'/scheduled_tasks/[:resource_id]', Handler.Resource,
-          Keyword.put(opts, :schema, Ada.Schema.ScheduledTask)},
+         collection_path('/locations', Ada.Schema.Location, opts),
+         resource_path('/locations', Ada.Schema.Location, opts),
+         collection_path('/users', Ada.Schema.User, opts),
+         resource_path('/users', Ada.Schema.User, opts),
+         collection_path('/scheduled_tasks', Ada.Schema.ScheduledTask, opts),
+         resource_path('/scheduled_tasks', Ada.Schema.ScheduledTask, opts),
          {'/workflows', Handler.Workflows, opts},
          {"/", :cowboy_static, {:priv_file, :ada, 'static/web-ui/index.html'}},
          {"/[...]", :cowboy_static, {:priv_dir, :ada, 'static/web-ui'}}
        ]}
     ])
+  end
+
+  defp collection_path(path, schema, opts) do
+    {path, Handler.Collection, Keyword.put(opts, :schema, schema)}
+  end
+
+  defp resource_path(path, schema, opts) do
+    {path ++ '/[:resource_id]', Handler.Resource, Keyword.put(opts, :schema, schema)}
   end
 end
