0e9e38751cbf1315e3bec6f6bc6cf77b6e0c66b1
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-10-12 12:23:39 +0100

    [F] Consume all collection APIs to update local state
    - GET /workflows
    - GET /users
    - GET /locations
    - GET /scheduled_tasks

--- web-ui/src/Main.elm
+++ web-ui/src/Main.elm
@@ -1,9 +1,257 @@
 module Main exposing (main)
 
 import Browser exposing (Document)
-import Html exposing (h1, text)
+import Dict as Dict
+import Html exposing (code, h1, text)
+import Http as Http
+import Json.Decode as JD
 import Platform.Cmd as Cmd
 import Platform.Sub as Sub
+import RemoteData exposing (..)
+
+
+-- RESOURCE TYPES
+
+
+type alias User =
+    { id : Int
+    , name : String
+    , email : String
+    }
+
+
+type alias Location =
+    { id : Int
+    , name : String
+    , lat : Float
+    , lng : Float
+    }
+
+
+
+-- SCHEDULE
+
+
+type alias Hour =
+    Int
+
+
+type alias Minute =
+    Int
+
+
+type alias Second =
+    Int
+
+
+type Frequency
+    = Daily Hour Minute
+    | Hourly Minute Second
+    | UnsupportedFrequency
+
+
+
+-- WORKFLOWS
+
+
+type WorkflowParam
+    = UserId Int
+    | LocationId Int
+    | NewsTag String
+    | UnsupportedParam
+
+
+type WorkflowRequirement
+    = RequiresUserId
+    | RequiresLocationId
+    | RequiresNewsTag
+    | UnsupportedRequirement
+
+
+type alias Workflow =
+    { name : String
+    , requirements : List WorkflowRequirement
+    }
+
+
+
+-- SCHEDULED TASKS
+
+
+type alias ScheduledTask =
+    { id : Int
+    , frequency : Frequency
+    , name : String
+    , params : List WorkflowParam
+    }
+
+
+
+-- API - USERS
+
+
+decodeUser : JD.Decoder User
+decodeUser =
+    JD.map3 User
+        (JD.field "id" JD.int)
+        (JD.field "name" JD.string)
+        (JD.field "email" JD.string)
+
+
+decodeUsers : JD.Decoder (List User)
+decodeUsers =
+    JD.list decodeUser
+
+
+getUsers : Cmd Msg
+getUsers =
+    Http.get "/users" decodeUsers
+        |> RemoteData.sendRequest
+        |> Cmd.map UsersResponse
+
+
+
+-- API - LOCATIONS
+
+
+decodeLocation : JD.Decoder Location
+decodeLocation =
+    JD.map4 Location
+        (JD.field "id" JD.int)
+        (JD.field "name" JD.string)
+        (JD.field "lat" JD.float)
+        (JD.field "lng" JD.float)
+
+
+decodeLocations : JD.Decoder (List Location)
+decodeLocations =
+    JD.list decodeLocation
+
+
+getLocations : Cmd Msg
+getLocations =
+    Http.get "/locations" decodeLocations
+        |> RemoteData.sendRequest
+        |> Cmd.map LocationsResponse
+
+
+
+-- API - WORKFLOWS
+
+
+requirementDecoder : JD.Decoder WorkflowRequirement
+requirementDecoder =
+    let
+        toRequirement reqName =
+            case reqName of
+                "tag" ->
+                    JD.succeed RequiresNewsTag
+
+                "user_id" ->
+                    JD.succeed RequiresUserId
+
+                "location_id" ->
+                    JD.succeed RequiresLocationId
+
+                otherwise ->
+                    JD.succeed UnsupportedRequirement
+    in
+    JD.string
+        |> JD.andThen toRequirement
+
+
+decodeWorkflow : JD.Decoder Workflow
+decodeWorkflow =
+    JD.map2 Workflow
+        (JD.field "name" JD.string)
+        (JD.field "requirements" (JD.list requirementDecoder))
+
+
+decodeWorkflows : JD.Decoder (List Workflow)
+decodeWorkflows =
+    JD.list decodeWorkflow
+
+
+getWorkflows : Cmd Msg
+getWorkflows =
+    Http.get "/workflows" decodeWorkflows
+        |> RemoteData.sendRequest
+        |> Cmd.map WorkflowsResponse
+
+
+
+-- API - SCHEDULED TASKS
+
+
+frequencyDecoder : JD.Decoder Frequency
+frequencyDecoder =
+    let
+        byFrequencyType freqType =
+            case freqType of
+                "daily" ->
+                    JD.map2 Daily
+                        (JD.field "hour" JD.int)
+                        (JD.field "minute" JD.int)
+
+                "hourly" ->
+                    JD.map2 Hourly
+                        (JD.field "minute" JD.int)
+                        (JD.field "second" JD.int)
+
+                other ->
+                    JD.succeed UnsupportedFrequency
+    in
+    JD.field "type" JD.string
+        |> JD.andThen byFrequencyType
+
+
+workflowParamDecoder : JD.Decoder WorkflowParam
+workflowParamDecoder =
+    let
+        toParam name =
+            case name of
+                "user_id" ->
+                    JD.map UserId
+                        (JD.field "value" JD.int)
+
+                "location_id" ->
+                    JD.map LocationId
+                        (JD.field "value" JD.int)
+
+                "tag" ->
+                    JD.map NewsTag
+                        (JD.field "value" JD.string)
+
+                other ->
+                    JD.succeed UnsupportedParam
+    in
+    JD.field "name" JD.string
+        |> JD.andThen toParam
+
+
+decodeScheduledTask : JD.Decoder ScheduledTask
+decodeScheduledTask =
+    JD.map4 ScheduledTask
+        (JD.field "id" JD.int)
+        (JD.field "frequency" frequencyDecoder)
+        (JD.field "workflow_name" JD.string)
+        (JD.field "params" (JD.list workflowParamDecoder))
+
+
+decodeScheduledTasks : JD.Decoder (List ScheduledTask)
+decodeScheduledTasks =
+    JD.list decodeScheduledTask
+
+
+getScheduledTasks : Cmd Msg
+getScheduledTasks =
+    Http.get "/scheduled_tasks" decodeScheduledTasks
+        |> RemoteData.sendRequest
+        |> Cmd.map ScheduledTasksResponse
+
+
+
+-- APPLICATION WIRING
 
 
 type alias Flags =
@@ -12,10 +260,19 @@ type alias Flags =
 
 type Msg
     = NoOp
+    | UsersResponse (WebData (List User))
+    | LocationsResponse (WebData (List Location))
+    | WorkflowsResponse (WebData (List Workflow))
+    | ScheduledTasksResponse (WebData (List ScheduledTask))
 
 
 type alias Model =
-    { count : Int }
+    { count : Int
+    , users : WebData (List User)
+    , locations : WebData (List Location)
+    , workflows : WebData (List Workflow)
+    , scheduledTasks : WebData (List ScheduledTask)
+    }
 
 
 main : Program Flags Model Msg
@@ -30,7 +287,14 @@ main =
 
 init : Flags -> ( Model, Cmd Msg )
 init initialCount =
-    ( { count = initialCount }, Cmd.none )
+    ( { count = initialCount
+      , users = NotAsked
+      , locations = NotAsked
+      , workflows = NotAsked
+      , scheduledTasks = NotAsked
+      }
+    , Cmd.batch [ getUsers, getLocations, getWorkflows, getScheduledTasks ]
+    )
 
 
 view : Model -> Document Msg
@@ -38,13 +302,30 @@ view model =
     { title = "Ada"
     , body =
         [ h1 [] [ text "Management UI" ]
+        , code []
+            [ text <| Debug.toString model
+            ]
         ]
     }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        UsersResponse response ->
+            ( { model | users = response }, Cmd.none )
+
+        LocationsResponse response ->
+            ( { model | locations = response }, Cmd.none )
+
+        WorkflowsResponse response ->
+            ( { model | workflows = response }, Cmd.none )
+
+        ScheduledTasksResponse response ->
+            ( { model | scheduledTasks = response }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
