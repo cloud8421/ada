85de42869e858af18fd29a40e830e4964adf64ab
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-10-18 09:18:19 +0100

    [B] Test ScheduledTask with local time

--- test/ada/schema/scheduled_task_test.exs
+++ test/ada/schema/scheduled_task_test.exs
@@ -23,10 +23,11 @@ defmodule Ada.Schema.ScheduledTaskTest do
     test "daily frequency" do
       st = %ScheduledTask{frequency: %Frequency{}}
 
-      assert ScheduledTask.matches_time?(st, ~N[2018-10-06 00:00:00.066161])
-      refute ScheduledTask.matches_time?(st, ~N[2018-10-06 00:00:01.066161])
-      refute ScheduledTask.matches_time?(st, ~N[2018-10-06 01:00:00.066161])
-      refute ScheduledTask.matches_time?(st, ~N[2018-10-06 14:01:00.066161])
+      # Local time is Europe London, so we declare an hour in advance
+      assert ScheduledTask.matches_time?(st, to_local(~N[2018-10-05 23:00:00.066161]))
+      refute ScheduledTask.matches_time?(st, to_local(~N[2018-10-06 00:00:01.066161]))
+      refute ScheduledTask.matches_time?(st, to_local(~N[2018-10-06 01:00:00.066161]))
+      refute ScheduledTask.matches_time?(st, to_local(~N[2018-10-06 14:01:00.066161]))
     end
 
     test "hourly frequency" do
@@ -34,10 +35,10 @@ defmodule Ada.Schema.ScheduledTaskTest do
         frequency: %Frequency{type: "hourly", minute: 6, second: 30}
       }
 
-      assert ScheduledTask.matches_time?(st, ~N[2018-10-06 00:06:30.066161])
-      refute ScheduledTask.matches_time?(st, ~N[2018-10-06 04:06:31.066161])
-      refute ScheduledTask.matches_time?(st, ~N[2018-10-06 14:01:30.066161])
-      refute ScheduledTask.matches_time?(st, ~N[2018-10-06 06:00:00.066161])
+      assert ScheduledTask.matches_time?(st, to_local(~N[2018-10-06 00:06:30.066161]))
+      refute ScheduledTask.matches_time?(st, to_local(~N[2018-10-06 04:06:31.066161]))
+      refute ScheduledTask.matches_time?(st, to_local(~N[2018-10-06 14:01:30.066161]))
+      refute ScheduledTask.matches_time?(st, to_local(~N[2018-10-06 06:00:00.066161]))
     end
   end
 
@@ -47,5 +48,11 @@ defmodule Ada.Schema.ScheduledTaskTest do
 
       assert "ADA" == ScheduledTask.execute(st, [])
     end
+  end
+
+  defp to_local(naive_datetime) do
+    naive_datetime
+    |> DateTime.from_naive!("Etc/UTC")
+    |> Calendar.DateTime.shift_zone!("Europe/London")
   end
 end
