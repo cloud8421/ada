41e2272132473503cdeffe5636c156e2a7c34a85
Claudio Ortolina <claudio@pspdfkit.com>
2019-03-13 21:22:31 +0300

    [F] Pretty print clock in console logs

--- lib/ada/display/driver/dummy.ex
+++ lib/ada/display/driver/dummy.ex
@@ -6,7 +6,11 @@ defmodule Ada.Display.Driver.Dummy do
   @impl true
   def set_buffer(buffer) do
     Logger.debug(fn ->
-      "Dummy Display -> content: #{inspect(buffer)}"
+      """
+      Dummy Display -> content:
+
+      #{pretty_format(buffer)}
+      """
     end)
   end
 
@@ -22,5 +26,31 @@ defmodule Ada.Display.Driver.Dummy do
     Logger.debug(fn ->
       "Dummy Display -> set default brightness"
     end)
+  end
+
+  defp pretty_format(m) do
+    m
+    |> Enum.reverse()
+    |> Enum.map(fn r -> show_row(r) <> "\n" end)
+    |> add_horizontal_borders()
+    |> Enum.join("")
+  end
+
+  defp show_row(r) do
+    str =
+      r
+      |> Enum.map(fn
+        1 -> "X"
+        0 -> " "
+      end)
+      |> Enum.join("")
+
+    "|" <> str <> "|"
+  end
+
+  defp add_horizontal_borders([first_line | _other] = lines) do
+    width = String.length(first_line) - 1
+
+    [String.duplicate("_", width) <> "\n"] ++ lines ++ [String.duplicate("‾", width) <> "\n"]
   end
 end
