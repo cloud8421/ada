a486a73f672a16fdbb3cfb4e3aa63304a3bd8b9e
Claudio Ortolina <claudio@pspdfkit.com>
2019-03-14 11:19:25 +0300

    [F] Improve formatting of CLI output
    Colors and better spacing

--- lib/ada/cli/format.ex
+++ lib/ada/cli/format.ex
@@ -2,17 +2,17 @@ defmodule Ada.CLI.Format do
   @break "\n"
 
   def list_users(users) do
-    preamble = "==> System users"
+    preamble = header("System Users")
 
     list =
       users
       |> Enum.map(fn user ->
-        """
-                 ID: #{user.id}
-               Name: #{user.name}
-              Email: #{user.email}
-            Last.fm: #{user.last_fm_username}
-        """
+        [
+          list_item("ID", user.id),
+          list_item("Name", user.name),
+          list_item("Email", user.email),
+          list_item("Last.fm", user.last_fm_username || "not set")
+        ]
       end)
       |> Enum.intersperse(@break)
 
@@ -20,65 +20,65 @@ defmodule Ada.CLI.Format do
   end
 
   def user_created({:ok, user}) do
-    "==> Created User with ID #{user.id}"
+    header("Created User with ID #{user.id}")
   end
 
   def user_created({:error, changeset}) do
-    "==> Error creating user: #{inspect(changeset.errors)}"
+    header("Error creating user: #{inspect(changeset.errors)}")
   end
 
   def user_updated({:ok, user}) do
-    "==> Updated User with ID #{user.id}"
+    header("Updated User with ID #{user.id}")
   end
 
   def user_updated({:error, changeset}) do
-    "==> Error updating user: #{inspect(changeset.errors)}"
+    header("Error updating user: #{inspect(changeset.errors)}")
   end
 
   def user_deleted({:ok, user}) do
-    "==> Deleted User with ID #{user.id}"
+    header("Deleted User with ID #{user.id}")
   end
 
   def user_deleted({:error, changeset}) do
-    "==> Error deleting user: #{inspect(changeset.errors)}"
+    header("Error deleting user: #{inspect(changeset.errors)}")
   end
 
   def brightness_changed(:ok) do
-    "==> Brightness updated successfully"
+    header("Brightness updated successfully")
   end
 
   def brightness_changed(error) do
-    "==> Error updating brightness: #{inspect(error)}"
+    header("Error updating brightness: #{inspect(error)}")
   end
 
   def location_created({:ok, location}) do
-    "==> Created location with ID #{location.id}"
+    header("Created location with ID #{location.id}")
   end
 
   def location_created({:error, changeset}) do
-    "==> Error creating location: #{inspect(changeset.errors)}"
+    header("Error creating location: #{inspect(changeset.errors)}")
   end
 
   def scheduled_task_created({:ok, scheduled_task}) do
-    "==> Created scheduled_task with ID #{scheduled_task.id}"
+    header("Created scheduled_task with ID #{scheduled_task.id}")
   end
 
   def scheduled_task_created({:error, changeset}) do
-    "==> Error creating scheduled_task: #{inspect(changeset.errors)}"
+    header("Error creating scheduled_task: #{inspect(changeset.errors)}")
   end
 
   def list_scheduled_tasks(scheduled_tasks) do
-    preamble = "==> Scheduled Tasks"
+    preamble = header("Scheduled Tasks")
 
     list =
       scheduled_tasks
       |> Enum.map(fn scheduled_task ->
-        """
-                         ID: #{scheduled_task.id}
-              Workflow Name: #{inspect(scheduled_task.workflow_name)}
-                     Params: #{format_params(scheduled_task.params)}
-                  Frequency: #{format_frequency(scheduled_task.frequency)}
-        """
+        [
+          list_item("ID", scheduled_task.id),
+          list_item("Workflow Name", inspect(scheduled_task.workflow_name)),
+          list_item("Params", format_params(scheduled_task.params)),
+          list_item("Frequency", format_frequency(scheduled_task.frequency))
+        ]
       end)
       |> Enum.intersperse(@break)
 
@@ -105,5 +105,13 @@ defmodule Ada.CLI.Format do
     {:ok, days} = Calendar.DefaultTranslations.weekday_names(:en)
 
     Enum.at(days, day_of_week - 1)
+  end
+
+  defp header(text) do
+    IO.ANSI.magenta() <> text <> IO.ANSI.reset()
+  end
+
+  defp list_item(k, v) do
+    "#{IO.ANSI.yellow()}#{k}: #{IO.ANSI.white()}#{v}#{IO.ANSI.reset()}\n"
   end
 end
