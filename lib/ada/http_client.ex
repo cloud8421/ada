caafc931bcd44a2a72b2d9221b0b0d58a6d695e4
Claudio Ortolina <claudio@pspdfkit.com>
2019-03-20 17:59:03 +0300

    [F] Instrument internal HTTP client

--- lib/ada/http_client.ex
+++ lib/ada/http_client.ex
@@ -7,12 +7,13 @@ defmodule Ada.HTTPClient do
     @moduledoc false
     defstruct status_code: 100,
               headers: [],
-              body: <<>>
+              body: <<>>,
+              duration: 0
   end
 
   defmodule ErrorResponse do
     @moduledoc false
-    defstruct message: nil
+    defstruct message: nil, duration: 0
   end
 
   def json_get(url, headers \\ [], qs_params \\ []) do
@@ -27,7 +28,7 @@ defmodule Ada.HTTPClient do
 
   def json_post(url, data, headers \\ []) do
     case post(url, Jason.encode!(data), headers, 'application/json') do
-      %Response{body: []} = response ->
+      %Response{body: <<>>} = response ->
         response
 
       %Response{} = response ->
@@ -40,7 +41,7 @@ defmodule Ada.HTTPClient do
 
   def json_put(url, data, headers \\ []) do
     case put(url, Jason.encode!(data), headers, 'application/json') do
-      %Response{body: []} = response ->
+      %Response{body: <<>>} = response ->
         response
 
       %Response{} = response ->
@@ -59,8 +60,12 @@ defmodule Ada.HTTPClient do
 
     url_with_qs = url <> "?" <> URI.encode_query(qs_params)
 
-    :httpc.request(:get, {String.to_charlist(url_with_qs), headers}, [], [])
-    |> process_response
+    meta = %{url: url_with_qs, method: :get}
+
+    :timer.tc(fn ->
+      :httpc.request(:get, {String.to_charlist(url_with_qs), headers}, [], body_format: :binary)
+    end)
+    |> process_response(meta)
   end
 
   def post(url, body, headers \\ [], content_type \\ 'application/json') do
@@ -69,8 +74,14 @@ defmodule Ada.HTTPClient do
         {String.to_charlist(k), String.to_charlist(v)}
       end)
 
-    :httpc.request(:post, {String.to_charlist(url), headers, content_type, body}, [], [])
-    |> process_response
+    meta = %{url: url, method: :post}
+
+    :timer.tc(fn ->
+      :httpc.request(:post, {String.to_charlist(url), headers, content_type, body}, [],
+        body_format: :binary
+      )
+    end)
+    |> process_response(meta)
   end
 
   def put(url, body, headers \\ [], content_type \\ 'application/json') do
@@ -79,8 +90,14 @@ defmodule Ada.HTTPClient do
         {String.to_charlist(k), String.to_charlist(v)}
       end)
 
-    :httpc.request(:put, {String.to_charlist(url), headers, content_type, body}, [], [])
-    |> process_response
+    meta = %{url: url, method: :put}
+
+    :timer.tc(fn ->
+      :httpc.request(:put, {String.to_charlist(url), headers, content_type, body}, [],
+        body_format: :binary
+      )
+    end)
+    |> process_response(meta)
   end
 
   def delete(url, headers \\ []) do
@@ -89,22 +106,61 @@ defmodule Ada.HTTPClient do
         {String.to_charlist(k), String.to_charlist(v)}
       end)
 
-    :httpc.request(:delete, {String.to_charlist(url), headers, [], []}, [], [])
-    |> process_response
+    meta = %{url: url, method: :delete}
+
+    :timer.tc(fn ->
+      :httpc.request(:delete, {String.to_charlist(url), headers, [], []}, [], body_format: :binary)
+    end)
+    |> process_response(meta)
   end
 
-  defp process_response({:ok, result}) do
+  defp process_response({elapsed_us, {:ok, result}}, meta) do
     {{_, status, _}, headers, body} = result
+    duration = to_ms(elapsed_us)
+
+    track_success(meta, status, body, duration)
 
     headers =
       Enum.map(headers, fn {k, v} ->
         {List.to_string(k), List.to_string(v)}
       end)
 
-    %Response{status_code: status, headers: headers, body: body}
+    %Response{status_code: status, headers: headers, body: body, duration: duration}
   end
 
-  defp process_response({:error, reason}) do
-    %ErrorResponse{message: reason}
+  defp process_response({elapsed_us, {:error, reason}}, meta) do
+    duration = to_ms(elapsed_us)
+    track_failure(reason, meta, duration)
+    %ErrorResponse{message: reason, duration: duration}
+  end
+
+  defp to_ms(us), do: div(us, 1000)
+
+  defp track_success(meta, status, body, duration) do
+    uri = URI.parse(meta.url)
+
+    metrics_meta = %{
+      host: uri.host,
+      method: meta.method,
+      status: status
+    }
+
+    :telemetry.execute(
+      [:http_client, :request, :ok],
+      %{duration: duration, resp_size: byte_size(body)},
+      metrics_meta
+    )
+  end
+
+  defp track_failure(reason, meta, duration) do
+    uri = URI.parse(meta.url)
+
+    metrics_meta = %{
+      host: uri.host,
+      method: meta.method,
+      reason: reason
+    }
+
+    :telemetry.execute([:http_client, :request, :error], %{duration: duration}, metrics_meta)
   end
 end
