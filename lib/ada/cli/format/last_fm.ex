5c700ba26acb8b19c06cee443d23553f6e376ab9
Claudio Ortolina <claudio@pspdfkit.com>
2019-03-27 10:02:28 +0300

    [F] Readable and colorful formatting of last.fm report

--- lib/ada/cli/format/last_fm.ex
+++ lib/ada/cli/format/last_fm.ex
@@ -5,47 +5,71 @@ defmodule Ada.CLI.Format.LastFm do
   @space " "
   @count_item "█"
   @left_pad "  "
+  @dash "-"
+  @emdash "—"
+  @ellipsis "…"
 
   def format_report(report) do
-    title = "Last.fm report"
-    report_datetime = format_report_date(report.local_now)
-
     [
-      [
-        @left_pad,
-        ANSI.red(),
-        "Last.fm report",
-        ANSI.reset(),
-        String.pad_leading(report_datetime, 80 - String.length(@left_pad <> title)),
-        @break
-      ],
+      title(report.local_now),
       @break,
-      [
-        @left_pad,
-        ANSI.cyan(),
-        "Most listened artist",
-        @break,
-        @left_pad,
-        ANSI.reset(),
-        report.most_listened_artist,
-        ANSI.reset(),
-        @break
-      ],
-      [
-        @left_pad,
-        ANSI.cyan(),
-        "Count by hour",
-        ANSI.reset(),
-        @break,
-        format_count_by_hour(report.count_by_hour),
-        ANSI.reset(),
-        @break
-      ]
+      format_most_listened_artist(report.most_listened_artist),
+      @break,
+      format_count_by_hour(report.count_by_hour),
+      @break,
+      format_tracks(report.tracks)
     ]
     |> :erlang.iolist_to_binary()
   end
 
+  defp title(local_now) do
+    title = "Last.fm report"
+    report_datetime = format_report_date(local_now)
+
+    [
+      @break,
+      @left_pad,
+      ANSI.red(),
+      title,
+      ANSI.reset(),
+      @space,
+      String.pad_leading(
+        @space <> report_datetime,
+        80 - String.length(@left_pad <> title),
+        @emdash
+      ),
+      @break
+    ]
+  end
+
+  defp format_most_listened_artist(most_listened_artist) do
+    [
+      @left_pad,
+      ANSI.cyan(),
+      "Most listened artist",
+      @break,
+      @left_pad,
+      ANSI.reset(),
+      most_listened_artist,
+      ANSI.reset(),
+      @break
+    ]
+  end
+
   defp format_count_by_hour(count_by_hour) do
+    [
+      @left_pad,
+      ANSI.cyan(),
+      "Count by hour",
+      ANSI.reset(),
+      @break,
+      format_count_by_hour_items(count_by_hour),
+      ANSI.reset(),
+      @break
+    ]
+  end
+
+  def format_count_by_hour_items(count_by_hour) do
     Enum.map(count_by_hour, fn {dt, count} ->
       [
         @left_pad,
@@ -53,6 +77,8 @@ defmodule Ada.CLI.Format.LastFm do
         @space,
         ANSI.white(),
         String.duplicate(@count_item, count),
+        @space,
+        Integer.to_string(count),
         ANSI.reset(),
         @break
       ]
@@ -65,5 +91,53 @@ defmodule Ada.CLI.Format.LastFm do
 
   defp format_hour(dt) do
     Calendar.Strftime.strftime!(dt, "%x, %H:00")
+  end
+
+  defp format_tracks(tracks) do
+    [
+      @left_pad,
+      ANSI.cyan(),
+      "Tracks",
+      ANSI.reset(),
+      @break,
+      format_track_items(tracks)
+    ]
+  end
+
+  defp format_track_items(tracks) do
+    Enum.map(tracks, fn t ->
+      [
+        @left_pad,
+        @dash,
+        @space,
+        ANSI.yellow(),
+        t.artist,
+        ANSI.reset(),
+        @space,
+        @emdash,
+        @space,
+        ellipsis(t.album, 76 - String.length(t.artist)),
+        @break,
+        @left_pad,
+        @left_pad,
+        ANSI.green(),
+        ellipsis(t.name, 76),
+        ANSI.reset(),
+        @break
+      ]
+    end)
+  end
+
+  defp ellipsis(string, max_length) do
+    if String.length(string) >= max_length do
+      truncated_string =
+        string
+        |> String.slice(0, max_length - 1)
+        |> String.trim_trailing()
+
+      truncated_string <> @ellipsis
+    else
+      string
+    end
   end
 end
