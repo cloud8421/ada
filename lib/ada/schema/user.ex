68fdff583f35443489d67e18909438400e0c9544
Claudio Ortolina <claudio@pspdfkit.com>
2019-03-31 16:39:03 +0300

    [F] Add gravatar url to users

--- lib/ada/schema/user.ex
+++ lib/ada/schema/user.ex
@@ -1,8 +1,6 @@
 defmodule Ada.Schema.User do
   use Ecto.Schema
 
-  @derive {Jason.Encoder, except: [:__struct__, :__meta__]}
-
   schema "users" do
     field :name, :string, null: false
     field :email, :string, null: false
@@ -15,5 +13,25 @@ defmodule Ada.Schema.User do
     user
     |> Ecto.Changeset.cast(params, [:name, :email, :last_fm_username])
     |> Ecto.Changeset.validate_required([:name, :email])
+  end
+
+  def gravatar_url(user) do
+    "https://www.gravatar.com/avatar/" <> md5(user.email)
+  end
+
+  defp md5(string) do
+    :crypto.hash(:md5, string) |> Base.encode16(case: :lower)
+  end
+end
+
+defimpl Jason.Encoder, for: Ada.Schema.User do
+  def encode(user, opts) do
+    data =
+      user
+      |> Map.from_struct()
+      |> Map.delete(:__meta__)
+      |> Map.put(:gravatar_url, Ada.Schema.User.gravatar_url(user))
+
+    Jason.Encode.map(data, opts)
   end
 end
