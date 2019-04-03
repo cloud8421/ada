5632af27dfeb96f7c1f04252de37f3488c47b586
Claudio Ortolina <claudio@pspdfkit.com>
2019-04-03 16:23:49 +0300

    [F] Log requests at info level

--- lib/ada/http/listener.ex
+++ lib/ada/http/listener.ex
@@ -1,4 +1,6 @@
 defmodule Ada.HTTP.Listener do
+  require Logger
+
   def child_spec(ctx) do
     http_port = Keyword.fetch!(ctx, :http_port)
 
@@ -13,12 +15,17 @@ defmodule Ada.HTTP.Listener do
              env: %{
                dispatch: Ada.HTTP.Router.dispatch(ctx)
              },
-             metrics_callback: &track_metrics/1,
+             metrics_callback: &track_request/1,
              stream_handlers: [:cowboy_metrics_h, :cowboy_compress_h, :cowboy_stream_h]
            }
          ]},
       type: :worker
     }
+  end
+
+  defp track_request(cowboy_metrics) do
+    track_metrics(cowboy_metrics)
+    log_request(cowboy_metrics)
   end
 
   defp track_metrics(cowboy_metrics) do
@@ -38,5 +45,22 @@ defmodule Ada.HTTP.Listener do
       _other_reason ->
         :telemetry.execute([:http_server, :request, :error], %{duration: duration_us}, meta)
     end
+  end
+
+  defp log_request(cowboy_metrics) do
+    Logger.info(fn ->
+      duration_ms =
+        System.convert_time_unit(
+          cowboy_metrics.req_end - cowboy_metrics.req_start,
+          :native,
+          :millisecond
+        )
+
+      req = cowboy_metrics.req
+
+      "method=#{req.method} path=#{req.path} status=#{cowboy_metrics.resp_status} duration=#{
+        duration_ms
+      }"
+    end)
   end
 end
