9e3eee9523ab0a37bd7cd2f0a2d61f33f90f5d0a
Claudio Ortolina <claudio@pspdfkit.com>
2019-04-24 09:17:14 +0300

    [D] Document CLI helpers module

--- lib/ada/cli/helpers.ex
+++ lib/ada/cli/helpers.ex
@@ -1,6 +1,11 @@
 defmodule Ada.CLI.Helpers do
-  @moduledoc false
+  @moduledoc """
+  Provides helper functions to ease the composition of CLI tasks.
+  """
 
+  @doc """
+  Uses the erlang distribution to connect the CLI node to the device node.
+  """
   def connect!(cli_node, target_node) when is_binary(target_node) do
     connect!(cli_node, String.to_atom(target_node))
   end
@@ -16,6 +21,10 @@ defmodule Ada.CLI.Helpers do
     true = Node.connect(target_node)
   end
 
+  @doc """
+  Only working on Mac, checks for the dependencies needed to infer
+  the current location of the machine from the command line.
+  """
   def ensure_location_dependencies! do
     case :os.find_executable('CoreLocationCLI') do
       false ->
@@ -34,6 +43,9 @@ defmodule Ada.CLI.Helpers do
     end
   end
 
+  @doc """
+  Only working on Mac, returns the current location data.
+  """
   def get_current_location_data do
     {data, 0} = System.cmd("CoreLocationCLI", ["-format", "%latitude|%longitude|%address"])
 
