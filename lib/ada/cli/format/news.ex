b300de9cc0b0914d61ed3f87cc37c3021155a14d
Claudio Ortolina <claudio@pspdfkit.com>
2019-04-30 17:49:39 +0300

    [B] Truncate news titles in CLI

--- lib/ada/cli/format/news.ex
+++ lib/ada/cli/format/news.ex
@@ -20,7 +20,7 @@ defmodule Ada.CLI.Format.News do
 
   defp format_story(story) do
     [
-      Markup.h1(story.title),
+      Markup.h1(Markup.ellipsis(story.title, 72)),
       Markup.h2(format_news_pub_date(story.pub_date)),
       Markup.h3(Markup.ellipsis(story.url, 72)),
       Markup.p(story.body_text, 72),
