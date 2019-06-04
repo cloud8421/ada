794e3e126364ea6119a1f1363d500be3c33f8462
Claudio Ortolina <claudio@pspdfkit.com>
2019-06-04 17:24:52 +0100

    [R/D] Improve documentation for display module
    Also makes `is_valid_cycle_spec/1` private

--- lib/ada/display.ex
+++ lib/ada/display.ex
@@ -1,22 +1,6 @@
 defmodule Ada.Display do
   @moduledoc """
-  Supports the following contents:
-
-  ## Static
-
-      Ada.Display.set_content({:static, Ada.UI.Helpers.chars_to_matrix('1234')})
-
-  ## Cyclic
-
-      Ada.Display.set_content(
-        {:cycle,
-         [
-           {Ada.UI.Helpers.chars_to_matrix('A   '), 200},
-           {Ada.UI.Helpers.chars_to_matrix(' A  '), 200},
-           {Ada.UI.Helpers.chars_to_matrix('  A '), 200},
-           {Ada.UI.Helpers.chars_to_matrix('   A'), 200}
-         ]}
-      )
+  Lower level driver to directly control the contents of the LED screen.
   """
 
   @behaviour :gen_statem
@@ -25,30 +9,81 @@ defmodule Ada.Display do
             brightness: 1,
             content: nil
 
+  @type duration_ms :: pos_integer()
+  @typedoc """
+  Represents a specification for a static screen, constantly displaying the
+  same contents.
+
+  For example:
+
+      {:static, Ada.UI.Helpers.chars_to_matrix('1234')}
+  """
+  @type static_spec :: {:static, Ada.Display.Driver.buffer()}
+
+  @typedoc """
+  Represents a specification for a cyclic screen, which loops over different
+  buffers, each one with a specific duration.
+
+  For example:
+
+      [
+        {Ada.UI.Helpers.chars_to_matrix('A   '), 200},
+        {Ada.UI.Helpers.chars_to_matrix(' A  '), 200},
+        {Ada.UI.Helpers.chars_to_matrix('  A '), 200},
+        {Ada.UI.Helpers.chars_to_matrix('   A'), 200}
+      ]
+  """
+  @type cycle_spec :: {:cycle, [{Ada.Display.Driver.buffer(), duration_ms()}]}
+  @type content :: static_spec() | cycle_spec()
+
   ################################################################################
   ################################## PUBLIC API ##################################
   ################################################################################
 
+  @doc false
+  @spec start_link(Keyword.t()) :: {:ok, pid()}
   def start_link(opts) do
     :gen_statem.start_link({:local, __MODULE__}, __MODULE__, opts, [])
   end
 
+  @doc """
+  Turn on the screen.
+  """
+  @spec turn_on() :: :ok
   def turn_on do
     :gen_statem.call(__MODULE__, :turn_on)
   end
 
+  @doc """
+  Turn off the screen.
+  """
+  @spec turn_off() :: :ok
   def turn_off do
     :gen_statem.call(__MODULE__, :turn_off)
   end
 
+  @doc """
+  Sets the screen contents.
+
+  Please see `t:static_spec/0` and `t:cycle_spec/0` for information about supported formats.
+  """
+  @spec set_content(content()) :: :ok | {:error, :cycle_spec_too_short} | no_return()
   def set_content(content) do
     :gen_statem.call(__MODULE__, {:set_content, content})
   end
 
+  @doc """
+  Sets the screen brigthness.
+  """
+  @spec set_brightness(Ada.Display.Driver.brightness()) :: :ok | no_return()
   def set_brightness(brightness) when brightness in 0..255 do
     :gen_statem.call(__MODULE__, {:set_brightness, brightness})
   end
 
+  @doc """
+  Gets the screen brigthness.
+  """
+  @spec get_brightness() :: Ada.Display.Driver.brightness()
   def get_brightness do
     :gen_statem.call(__MODULE__, :get_brightness)
   end
@@ -57,12 +92,13 @@ defmodule Ada.Display do
   #################################### UTILS #####################################
   ################################################################################
 
-  defguard is_valid_cycle_spec(cycle_spec) when length(cycle_spec) >= 2
+  defguardp is_valid_cycle_spec(cycle_spec) when length(cycle_spec) >= 2
 
   ################################################################################
   ################################## CALLBACKS ###################################
   ################################################################################
 
+  @doc false
   def child_spec(opts) do
     %{
       id: __MODULE__,
@@ -73,8 +109,10 @@ defmodule Ada.Display do
     }
   end
 
+  @doc false
   def callback_mode, do: :state_functions
 
+  @doc false
   def init(opts) do
     driver = Keyword.get(opts, :driver, DummyDriver)
 
@@ -85,6 +123,7 @@ defmodule Ada.Display do
 
   # OFF
 
+  @doc false
   def off({:call, from}, :turn_on, data) do
     :ok = data.driver.set_buffer(data.content)
     :ok = data.driver.set_default_brightness()
@@ -128,6 +167,7 @@ defmodule Ada.Display do
 
   # STATIC
 
+  @doc false
   def static({:call, from}, :turn_on, _data) do
     {:keep_state_and_data, {:reply, from, :ok}}
   end
@@ -167,6 +207,7 @@ defmodule Ada.Display do
 
   # CYCLIC
 
+  @doc false
   def cyclic(event_type, :cycle, data)
       when event_type in [:internal, :state_timeout] do
     {{:value, {buffer, duration} = spec_item}, remaining_spec} = :queue.out(data.content)
