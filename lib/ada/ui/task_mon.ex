adf1cefd45e00b14b6f6ad4e5ec466111d49e458
Claudio Ortolina <claudio@pspdfkit.com>
2019-06-10 16:53:34 +0100

    [F] More playful display for running tasks

--- lib/ada/ui/task_mon.ex
+++ lib/ada/ui/task_mon.ex
@@ -2,22 +2,17 @@ defmodule Ada.UI.TaskMon do
   @moduledoc false
   alias Ada.UI.Helpers
 
+  @busy {:cycle,
+         [
+           {Helpers.chars_to_matrix([:block, :square, :dash, :space]), 50},
+           {Helpers.chars_to_matrix([:square, :block, :square, :dash]), 50},
+           {Helpers.chars_to_matrix([:dash, :square, :block, :square]), 50},
+           {Helpers.chars_to_matrix([:square, :dash, :square, :block]), 50}
+         ]}
+
+  @finished {:static, Helpers.chars_to_matrix([:block, :block, :block, :block])}
+
   def render(running_tasks) do
-    content =
-      running_tasks
-      |> running_count
-      |> Helpers.chars_to_matrix()
-
-    {:static, content}
-  end
-
-  defp running_count(running_tasks) do
-    padded_count =
-      running_tasks
-      |> MapSet.size()
-      |> Integer.digits()
-      |> Helpers.pad_with_space()
-
-    'RT' ++ padded_count
+    if MapSet.size(running_tasks) == 0, do: @finished, else: @busy
   end
 end
